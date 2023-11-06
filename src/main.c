fdc43600746f7ac004ef762b765f0843712d29be
Jon <jharder@kipsu.com>
2023-11-06 07:02:07 -0600

    Starting to add command line parse options

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include "todo_list.h"
 
+typedef enum bool_t {
+  false,
+  true,
+} bool;
+
+struct options_t {
+  bool clear;
+};
+
+void parse_args(struct options_t* options) {
+  options->clear = true;
+}
+
 int main(int argc, char *argv[]) {
+  struct options_t options;
+  // options.clear = false;
+  parse_args(&options);
+
   struct todo_list_t* todos = read_todos();
   for (int i=1; i<argc; i++) {
     struct todo_t* todo = create_todo(argv[i]);
